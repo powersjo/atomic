5318eef956a097b44ffb8d98cce97310dcf347d5
onekam <onekam@mail.gvsu.edu>
2015-10-24 17:28:44 -0400

    java file

--- OthelloGame/src/Game.java
+++ OthelloGame/src/Game.java
@@ -9,8 +9,8 @@ import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
 
 public class Game {
-	static int boardWidth, boardHeight, boardSize;
-	static CellState[] board;
+	public int boardWidth, boardHeight, boardSize;
+	public CellState[] board;
 	static ArrayList<Integer> blackCells, whiteCells, emptyCells;
 	static Set<Integer> possibleMoves;
 	static CellState enemy, me;
@@ -18,144 +18,314 @@ public class Game {
 	public Game(){
 		possibleMoves = new HashSet<Integer>();
 		board = new CellState[64];
+		enemy = CellState.WHITE;
+		me = CellState.BLACK; 
+
+
+		blackCells = new ArrayList<>();
+		whiteCells = new ArrayList<>();
 	}
-	
+
 	public boolean checkLeft(int i){
 		i--;
-		if (i%8 == 0){
+
+		if (i%8 == 0)
 			return false;
-		}
 		else if (board[i] == enemy){
+			//System.out.println(i);
 			return checkLeft(i);
 		}
-		else if (board[i] == me){
-			return false;
-		}
-		else{
-			// only executes when the given cell is empty
+		else if ((board[i] != enemy && board[i] != me) && board[i+1] == enemy){
+			//System.out.println("I got here");
 			possibleMoves.add(i);
 			return true;
 		}
+		else{	
+			return false;
+		}		
+
+		//		i--;
+		//		if (i%8 == 0){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkLeft(i);
+		//		}
+		//		else if (board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i+1] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			// only executes when the given cell is empty
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
 	}
-	
+
 	public boolean checkRight(int i){
 		i++;
-		if (i%8 == 7){
+
+		if (i%8 == 7)
 			return false;
-		}
 		else if (board[i] == enemy){
 			return checkRight(i);
 		}
-		else if (board[i] == me){
-			return false;
-		}
-		else{
-			// only executes when empty
+		else if (((board[i] != enemy && board[i] != me) && board[i-1] == enemy)){
 			possibleMoves.add(i);
 			return true;
 		}
-	}
-	
-	public boolean checkUp(int i){
-		i -= 8;
-		if (i < 8){
+		else{
 			return false;
 		}
+		//		i++;
+		//		if (i%8 == 7){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkRight(i);
+		//		}
+		//		else if (board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i-1] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			// only executes when empty
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
+	}
+
+	public boolean checkUp(int i){
+		i-=8;
+
+		if (i < 8)
+			return false;
 		else if (board[i] == enemy){
 			return checkUp(i);
 		}
-		else if (board[i] == me){
-			return false;
-		}
-		else{
+		else if ((board[i] != enemy && board[i] != me) && board[i+8] == enemy){
 			possibleMoves.add(i);
 			return true;
 		}
-	}
-	
-	public boolean checkDown(int i){
-		i += 8;
-		if (i > 55){
+		else{
 			return false;
 		}
+		//		i -= 8;
+		//		if (i < 8){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkUp(i);
+		//		}
+		//		else if (board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i+8] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
+	}
+
+	public boolean checkDown(int i){
+		i += 8;
+
+		if (i > 55)
+			return false;
 		else if (board[i] == enemy){
 			return checkDown(i);
 		}
-		else if (board[i] == me){
-			return false;
-		}
-		else{
+		else if ((board[i] != enemy && board[i] != me) && board[i-8] == enemy){
 			possibleMoves.add(i);
 			return true;
 		}
-	}
-	
-	public boolean checkDiagUpLeft(int i){
-		i -= 9;
-		if (i < 8 || i%8 == 0){
+		else{
 			return false;
 		}
+		//		i += 8;
+		//		if (i > 55){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkDown(i);
+		//		}
+		//		else if (board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i-8] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
+	}
+
+	public boolean checkDiagUpLeft(int i){
+		i -= 9;
+
+		if (i%8 == 0 || i < 8)
+			return false;
 		else if (board[i] == enemy){
 			return checkDiagUpLeft(i);
 		}
-		else if (board[i] == me){
-			return false;
-		}
-		else{
+		else if (((board[i] != enemy && board[i] != me) && board[i+9] == enemy)){
 			possibleMoves.add(i);
 			return true;
 		}
-	}
-	
-	public boolean checkDiagUpRight(int i){
-		i -= 7;
-		if (i < 8 || i%8 == 7){
+		else{
 			return false;
 		}
+		//		if (i < 8 || i%8 == 0){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkDiagUpLeft(i);
+		//		}
+		//		else if (board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i+9] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
+	}
+
+	public boolean checkDiagUpRight(int i){
+
+		i-=7;
+
+		if (i < 8 || i%8 == 7)
+			return false;
 		else if (board[i] == enemy){
 			return checkDiagUpRight(i);
 		}
-		else if(board[i] == me){
-			return false;
-		}
-		else{
+		else if (((board[i] != enemy && board[i] != me) && board[i+7] == enemy)){
 			possibleMoves.add(i);
 			return true;
 		}
+		else{
+			return false;
+		}
+		//		i -= 7;
+		//		if (i < 8 || i%8 == 7){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkDiagUpRight(i);
+		//		}
+		//		else if(board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i+7] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
 	}
-	
+
 	public boolean checkDiagDownLeft(int i){
-		i += 7;
-		if (i > 55 || i%8 == 0){
+		i+=7;
+
+		if (i>55 || i%8 == 0)
 			return false;
-		}
 		else if (board[i] == enemy){
 			return checkDiagDownLeft(i);
 		}
-		else if (board[i] == me){
-			return false;
-		}
-		else{
+		else if (((board[i] != enemy && board[i] != me) && board[i-7] == enemy)){
 			possibleMoves.add(i);
 			return true;
 		}
+		else{
+			return false;
+		}
+		//		i += 7;
+		//		if (i > 55 || i%8 == 0){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkDiagDownLeft(i);
+		//		}
+		//		else if (board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i-7] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
 	}
-	
+
 	public boolean checkDiagDownRight(int i){
+
 		i += 9;
-		if (i > 55 || i%8 == 7){
+
+		if (i > 55 || i%8 == 7)
 			return false;
-		}
 		else if (board[i] == enemy){
-			return checkDiagDownLeft(i);
+			return checkDiagDownRight(i);
 		}
-		else if(board[i] == me){
-			return false;
-		}
-		else{
+		else if (((board[i] != enemy && board[i] != me) && board[i-9] == enemy)){
 			possibleMoves.add(i);
 			return true;
 		}
+		else{
+			return false;
+		}
+		//		i += 9;
+		//		if (i > 55 || i%8 == 7){
+		//			return false;
+		//		}
+		//		else if (board[i] == enemy){
+		//			return checkDiagDownRight(i);
+		//		}
+		//		else if(board[i] == me){
+		//			return false;
+		//		}
+		//		else if (board[i-9] == me && board[i] == null){
+		//			return false;
+		//		}
+		//		else if (board[i] == null){
+		//			possibleMoves.add(i);
+		//			return true;
+		//		}
+		//		else{
+		//			return false;
+		//		}
 	}
 
 	public void checkCell(int i){
@@ -168,21 +338,29 @@ public class Game {
 		checkDiagDownLeft(i);
 		checkDiagDownRight(i);
 	}
-	
+
 	public int pickMove(){
-//		Random randomgenerator = new Random();
-//		int random = randomgenerator.nextInt(possibleMoves.size());
-//		
-//		Object x[] = possibleMoves.toArray();
-//		
-//		return (int)x[random];
-		
-		return possibleMoves.size();
+		Random randomgenerator = new Random();
+		int random = randomgenerator.nextInt(possibleMoves.size());
+
+		Object x[] = possibleMoves.toArray();
+
+		return (int)x[random];
+
+		//return possibleMoves.size();
 	}
 
-	public static int runMe(JSONObject myObj){
-		
-		Game g = new Game();
+	public CellState[] getBoard(){
+		return board;
+	}
+
+	public void setValue(int x, CellState cs){
+		board[x] = cs;
+	}
+
+	public static void runMe(JSONObject myObj){
+
+
 
 		JSONObject jsonObject = myObj;
 		int width = ((Long)jsonObject.get("width")).intValue();
@@ -195,57 +373,78 @@ public class Game {
 		System.out.println("Max index: " + max);
 		System.out.println("\nsquares:");
 		Iterator<String> iterator = squares.iterator();
-		
-		blackCells = new ArrayList<>();
-		whiteCells = new ArrayList<>();
-		
+
+		Game g2 = new Game();
+
+
 		int index = 0;
-		while (iterator.hasNext()) {
+		while (iterator.hasNext() && index < 64) {
 			if (squares.get(index).equals("b")){
-				board[index] = CellState.BLACK;
+				g2.setValue(index, CellState.BLACK);
 				blackCells.add(index);
 			}
 			else if (squares.get(index).equals("w")){
-				board[index] = CellState.WHITE;
+				g2.setValue(index, CellState.WHITE);
 				whiteCells.add(index);
 			}
-			else if (squares.get(index).equals("-")){
-				board[index] = CellState.EMPTY;
-			}
-			
-			if(index < 64){
-				index++;
-			}
-			System.out.println(iterator.next());
+
+			//if(index < 64){
+			index++;
+			//}
+			System.out.print(iterator.next());
 		}
 
-		enemy = CellState.WHITE;
-		me = CellState.BLACK; 
-		Game g2 = new Game();
-		
 		for (int i : blackCells){
 			g2.checkCell(i);
 		}
-		JSONParser parser = new JSONParser();
 
-		
-		
-		return g2.pickMove();
+		System.out.println(possibleMoves.toString());
+		System.exit(g2.pickMove());
 
 	}
 
 
 	public static void main (String[] args){
-		JSONParser parser = new JSONParser();
 		try{
-			Object obj = parser.parse(new FileReader(
-					"C:/Users/onekam/Documents/Othello/the_game/atomic/OthelloGame/bin/file1.txt"));
-			System.out.println(runMe((JSONObject)obj));
+			System.out.println("\n\n" + args[0]);
+			System.out.println(args[1]+"\n"+args[2]+"\n");
+			System.out.println(args[3]+"\n\n");
+
+			JSONParser parser = new JSONParser();
+			try{
+				//			Object obj = parser.parse(new FileReader(
+				//					"C:/Users/onekam/Documents/Othello/the_game/atomic/OthelloGame/bin/file1.txt"));
+				//			runMe((JSONObject)obj);
+
+
+				Object obj = parser.parse(args[1]);
+				String color = args[3];
+
+				if (color.equals("white")){
+					me = CellState.WHITE;
+					enemy = CellState.BLACK;
+				}
+				else if (color.equals("black")){
+					me = CellState.BLACK;
+					enemy = CellState.WHITE;
+				}
+				else{
+					System.exit(0);
+				}
+
+				runMe((JSONObject)obj);
+			}
+			catch (Exception e){
+				e.printStackTrace();
+			}
+
 		}
-		catch (Exception e){
-			e.printStackTrace();
+		catch (Exception e1){
+			System.out.println("");
 		}
-		
-		
+
+
+
 	}
+
 }

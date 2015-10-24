de7f21c412390cb9f690889cc35dc26138fb5200
onekam <onekam@mail.gvsu.edu>
2015-10-24 09:40:27 -0400

    initial

---
+++ Othello/src/BoardState.java
@@ -0,0 +1,4 @@
+
+public enum BoardState {
+	OPEN, BLACK, WHITE;
+}
---
+++ Othello/src/OthelloGame.java
@@ -0,0 +1,18 @@
+import org.json.*;
+
+public class OthelloGame {
+
+	int[] board;
+	
+	public void parse(int size){
+		board = new int[size];
+		for (int i = 0; i < obj.length(); i++){
+			
+		}
+	}
+	
+	
+	public static void main(String[] args){
+		
+	}
+}

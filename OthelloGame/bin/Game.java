dca068b5a4338d16bb1f23bdfee1ca339e304f44
Jon <powers.j.email@gmail.com>
2015-10-24 12:52:09 -0400

    it works

--- OthelloGame/bin/Game.java
+++ OthelloGame/bin/Game.java
@@ -1,19 +1,43 @@
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
+import java.io.StringReader;
+import java.io.FileReader;
+import java.util.Iterator;
+import org.json.simple.JSONObject;
 
 public class Game {
 	static int boardWidth, boardHeight, boardSize;
 	static CellState[] board;
-	
+
+	@SuppressWarnings("unchecked")
 	public static void main (String[] args){
 		JSONParser parser = new JSONParser();
 		
+		/////////////////
+
+		//////////////////
+
 		try {
-			
-			System.out.println(args[0]);
-           Object obj = parser.parse(args[0]);
+
+		///////////////
+
+		Object obj = parser.parse(new FileReader(
+                    "C:/Users/Jonathan/Downloads/atomic/atomic/OthelloGame/bin/file1.txt"));
  
-            JSONObject jsonObject = new JSONObject();
+            //JSONObject jsonObject = (JSONObject) obj;
+ 
+            
+
+		////////////////
+
+			//Object myObj = parser.parse(new FileReader(
+                    //"C:/Users/Jonathan/Downloads/atomic/atomic/OthelloGame/bin/test.JSON"));
+			//StringReader myArg = new StringReader(args[0]);
+			//System.out.println(args[0]);
+           //Object obj = parser.parse(String.valueOf(myArg));
+		   //StringReader myArg = new StringReader(args[0]);
+			runMe((JSONObject)obj);
+            /*JSONObject jsonObject = new JSONObject();
  
             String width = (String) jsonObject.get("width");
             boardWidth = Integer.parseInt(width);
@@ -35,11 +59,28 @@ public class Game {
             		board[i] = CellState.WHITE;
             	else
             		System.out.println("Not Found");
-            }
+            }*/
  
         } catch (Exception e) {
-            System.out.println("error");
+            e.printStackTrace();
         }
+
 		
+	}
+	public static void runMe(JSONObject myObj){
+		JSONObject jsonObject = myObj;
+		int width = ((Long)jsonObject.get("width")).intValue();
+        int height = ((Long)jsonObject.get("height")).intValue();
+		int max = ((Long)jsonObject.get("max-index")).intValue();
+        JSONArray squares = (JSONArray) jsonObject.get("squares");
+ 
+            System.out.println("width: " + width);
+            System.out.println("height: " + height);
+			System.out.println("Max index: " + max);
+            System.out.println("\nsquares:");
+            Iterator<String> iterator = squares.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
 	}
 }
